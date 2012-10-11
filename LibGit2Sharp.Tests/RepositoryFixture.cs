551c96321d1a49e15d00bbadf582a75c4a45a660
Ben Straub <bs@github.com>
2012-10-11 09:03:19 -0700

    Clone: switch to using libgit2's test repos
    Also add test case for git:// protocol.

--- LibGit2Sharp.Tests/RepositoryFixture.cs
+++ LibGit2Sharp.Tests/RepositoryFixture.cs
@@ -62,9 +62,10 @@ namespace LibGit2Sharp.Tests
         }
 
         [Theory]
-        [InlineData("http://github.com/nulltoken/TestGitRepository")]
-        [InlineData("https://github.com/nulltoken/TestGitRepository")]
-        //[InlineData("git@github.com:nulltoken/TestGitRepository")]
+        [InlineData("http://github.com/libgit2/TestGitRepository")]
+        [InlineData("https://github.com/libgit2/TestGitRepository")]
+        [InlineData("git://github.com/nulltoken/TestGitRepository")]
+        //[InlineData("git@github.com:libgit2/TestGitRepository")]
         public void CanCloneARepository(string url)
         {
             var scd = BuildSelfCleaningDirectory();
@@ -85,9 +86,10 @@ namespace LibGit2Sharp.Tests
         }
 
         [Theory]
-        [InlineData("http://github.com/nulltoken/TestGitRepository")]
-        [InlineData("https://github.com/nulltoken/TestGitRepository")]
-        //[InlineData("git@github.com:nulltoken/TestGitRepository")]
+        [InlineData("http://github.com/libgit2/TestGitRepository")]
+        [InlineData("https://github.com/libgit2/TestGitRepository")]
+        [InlineData("git://github.com/libgit2/TestGitRepository")]
+        //[InlineData("git@github.com:libgit2/TestGitRepository")]
         public void CanCloneARepositoryBarely(string url)
         {
             var scd = BuildSelfCleaningDirectory();
